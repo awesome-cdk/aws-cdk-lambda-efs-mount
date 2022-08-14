65f5e2c0a8727a8b0c7c6da3609830c1f23e03cb
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2022-08-14 16:00:06 +0300

    feat: Append request ID from AWS API Gateway to mounted file

--- cdk/lib/get.lambda.ts
+++ cdk/lib/get.lambda.ts
@@ -3,11 +3,12 @@ import * as fs from 'fs';
 import * as os from 'os';
 
 const logFile = '/mnt/request-logs/requests.txt';
+const separator = os.EOL;
 
 export const handler: APIGatewayProxyHandler = async (event) => {
-    fs.appendFileSync(logFile, event.requestContext.requestId + os.EOL);
+    fs.appendFileSync(logFile, event.requestContext.requestId + separator);
 
-    const rows = fs.readFileSync(logFile).toString().split(os.EOL);
+    const rows = fs.readFileSync(logFile).toString().split(separator);
 
     return {
         statusCode: 200,
