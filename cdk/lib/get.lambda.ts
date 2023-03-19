f5306ef8c82efeb2307375863bc46e709409864b
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2023-03-19 22:56:14 +0200

    feat: Wrap up

--- cdk/lib/get.lambda.ts
+++ cdk/lib/get.lambda.ts
@@ -2,7 +2,11 @@ import {APIGatewayProxyHandler} from "aws-lambda";
 import * as fs from 'fs';
 import * as os from 'os';
 
+// Where in the mounted filesystem to store the logs
+// This file persists across requests and Lambda cold-starts
 const logFile = '/mnt/request-logs/requests.txt';
+
+// Use the OS level line-ending char as separator
 const separator = os.EOL;
 
 export const handler: APIGatewayProxyHandler = async (event) => {
